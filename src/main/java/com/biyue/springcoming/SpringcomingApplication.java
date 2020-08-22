14ecf95c24609f765a5442249a3f7f62504fabf0
biyue001 <18961736622@163.com>
2020-08-22 18:15:30 +0800

    项目初始化

---
+++ src/main/java/com/biyue/springcoming/SpringcomingApplication.java
@@ -0,0 +1,13 @@
+package com.biyue.springcoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcomingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biyue/springcoming/SpringcomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biyue.springcoming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
